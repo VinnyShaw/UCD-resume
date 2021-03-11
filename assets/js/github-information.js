f9f9c4e9b9788a8975ba9fc2e1fac677b7ac72b6
Vinny Shaw <VinnyShaw@users.noreply.github.com>
2021-03-11 12:01:38 +0000

    render user information from guthub

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,22 @@
+// This function returns the users display name, avatar, followers, following and repos.
+function userInformationHTML(user) {
+  return `
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>`;
+}
+
+// This function checks if there is a username entered in to the text area.  If not it asks the user to enter a username
 function fetchGitHubInformation(event) {
   var username = $("#gh-username").val();
   if (!username) {
@@ -5,9 +24,30 @@ function fetchGitHubInformation(event) {
     return;
   }
 
+  // This displays the loading.gif
   $("#gh-user-data").html(
     `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`
+  );
+
+  // When a valid GitHub username is entered, th information will be stored in the variable UserInformationHTML
+  // If the username is not valid and returns an error 404, the error message "No info found for user..." will be displayed
+  // If there error is not a 404, the error will be console logged then displayed to the user
+  $.when($.getJSON(`https://api.github.com/users/${username}`)).then(
+    function (response) {
+      var userData = response;
+      $("#gh-user-data").html(userInformationHTML(userData));
+    },
+    function (errorResponse) {
+      if (errorResponse.status === 404) {
+        $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+      } else {
+        console.log(errorResponse);
+        $("#gh-user-data").html(
+          `<h2>Error: ${errorResponse.responseJSON.message}</h2>`
+        );
+      }
+    }
   );
 }
